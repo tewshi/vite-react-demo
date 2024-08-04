9717ceba8d7799dcd241d17df4b5010486635ffd
Joseph Ojoko <entewshi@gmail.com>
2024-08-04 19:55:49 +0100

    feat: apply stopwatch logic

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,38 +1,95 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Button from '@/components/Button'
 import styles from '@/components/stopwatch.module.css'
 
+enum StopwatchState {
+  idle,
+  running,
+  paused,
+}
+
 function Stopwatch() {
   const [time, setTime] = useState(0)
-  const [state, setState] = useState<'idle' | 'running' | 'paused'>('idle')
+  const intervalId = useRef(0)
+  const [state, setState] = useState<StopwatchState>(StopwatchState.idle)
+
+  useEffect(() => {
+    if (state === StopwatchState.running) {
+      intervalId.current = setInterval(() => {
+        setTime((t) => t + 10)
+      }, 10)
+    }
+
+    return () => clearInterval(intervalId.current)
+  }, [state])
 
   const onStart = () => {
-    setState('running')
+    setState(StopwatchState.running)
   }
 
   const onPause = () => {
-    setState('paused')
+    setState(StopwatchState.paused)
   }
 
   const onResume = () => {
-    setState('running')
+    setState(StopwatchState.running)
   }
 
   const onReset = () => {
-    setState('idle')
+    setState(StopwatchState.idle)
     setTime(0)
+  }
+
+  const formatTime = () => {
+    const hours = Math.floor(time / 1000 / 60 / 60)
+      .toString()
+      .padStart(2, '0')
+    const minutes = Math.floor((time / 1000 / 60) % 60)
+      .toString()
+      .padStart(2, '0')
+    const seconds = Math.floor((time / 1000) % 60)
+      .toString()
+      .padStart(2, '0')
+
+    return `${hours}:${minutes}:${seconds}`
   }
 
   return (
     <div className={styles.stopwatch}>
-      <div className={styles.time}>00:00:00</div>
+      <div className={styles.time}>{formatTime()}</div>
       <div className={styles.controls}>
-        <Button disabled={state !== 'idle'} onClick={onStart}>Start</Button>
-        {state !== 'paused' && <Button color="secondary" disabled={state === 'idle'} onClick={onPause}>Pause</Button>}
-        {state === 'paused' && <Button color="info" onClick={onResume}>Resume</Button>}
-        <Button color="destructive" disabled={state === 'idle'} onClick={onReset}>Reset</Button>
+        <Button
+          disabled={state !== StopwatchState.idle}
+          onClick={onStart}
+        >
+          Start
+        </Button>
+        {state !== StopwatchState.paused && (
+          <Button
+            color="secondary"
+            disabled={state === StopwatchState.idle}
+            onClick={onPause}
+          >
+            Pause
+          </Button>
+        )}
+        {state === StopwatchState.paused && (
+          <Button
+            color="info"
+            onClick={onResume}
+          >
+            Resume
+          </Button>
+        )}
+        <Button
+          color="destructive"
+          disabled={state === StopwatchState.idle}
+          onClick={onReset}
+        >
+          Reset
+        </Button>
       </div>
-    </div >
+    </div>
   )
 }
 
