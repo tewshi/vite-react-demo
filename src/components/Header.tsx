67defd132c77ee99c09206796fa4463c4dd7863e
Joseph Ojoko <entewshi@gmail.com>
2024-08-05 02:36:58 +0100

    feat: apply theme switcher

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,45 @@
+import { FC } from 'react'
+import {
+  MoonIcon,
+  SunIcon,
+  ComputerDesktopIcon,
+} from '@heroicons/react/24/solid'
 import styles from '@/components/header.module.css'
+import Button from '@/components/Button'
+import useThemeMode from '@/hooks/useThemeMode'
 
-function Header() {
+const Header: FC = () => {
+  const [mode, setMode] = useThemeMode()
+
   return (
     <header id={styles['app-header']}>
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Blog</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
         </ul>
       </nav>
+      <Button
+        className={styles['theme-switcher']}
+        color="info"
+        variant="outlined"
+        icon
+        onClick={() => setMode()}
+      >
+        {mode === 'light' && <SunIcon className="w-4" />}
+        {mode === 'dark' && <MoonIcon className="w-4" />}
+        {mode === 'system' && <ComputerDesktopIcon className="w-4" />}
+      </Button>
     </header>
   )
 }
