ece539078a69100ca9f4f0980ac1a9acc5c7bf53
Joseph Ojoko <entewshi@gmail.com>
2024-08-06 12:05:29 +0100

    chore: configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
@@ -5,14 +7,27 @@ import pluginReact from 'eslint-plugin-react'
 
 export default [
   { files: ['**/*.{js,ts,tsx}'] },
+  { ignores: ['*.config.js'] },
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: [
+          './tsconfig.json',
+          './tsconfig.node.json',
+          './tsconfig.app.json',
+        ],
+        tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
+      },
+    },
     settings: { react: { version: '^18.3' } },
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
 ]
